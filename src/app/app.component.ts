2975dc110f0c42128f03adac5b465deb9e9cb8ad
Ben <nalgbtqiaplus@gmail.com>
2025-02-09 16:16:39 +0100

    drawer formatting + close button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,36 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NavbarWebComponent } from './components/navbar-web/navbar-web.component';
-import { ContactBarComponent } from "./components/contact-bar/contact-bar.component";
+import { ContactBarComponent } from './components/contact-bar/contact-bar.component';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ResponsiveDirective } from './directives/responsive.directive';
 import { NavbarMobileComponent } from './components/navbar-mobile/navbar-mobile.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawer, MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [MatIconModule, MatSidenavModule, RouterOutlet, NavbarWebComponent, NavbarMobileComponent],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    RouterOutlet,
+    NavbarWebComponent,
+    NavbarMobileComponent,
+    ResponsiveDirective,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
+  @ViewChild('drawer') drawer!: MatDrawer;
+  
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     // Register the Facebook SVG icon
     iconRegistry.addSvgIcon(
-      'facebook', 
+      'facebook',
       sanitizer.bypassSecurityTrustResourceUrl('facebook.svg')
     );
   }
-
-   @ViewChild('navbarMobile') navbarMobile!: NavbarMobileComponent;
-
-   toggleDrawer() {
-    if (this.navbarMobile && this.navbarMobile.drawer) {
-       this.navbarMobile.drawer.toggle();
-    }
- }
 }
--- src/app/components/navbar-mobile/navbar-mobile.component.ts
+++ src/app/components/navbar-mobile/navbar-mobile.component.ts
@@ -1,18 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawer, MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { ResponsiveDirective } from '../../directives/responsive.directive';
+import { ContactBarComponent } from "../contact-bar/contact-bar.component";
 
 @Component({
   selector: 'app-navbar-mobile',
-  imports: [MatSidenavModule, MatButtonModule, MatIconModule, RouterLink, RouterLinkActive, ResponsiveDirective],
+  imports: [ContactBarComponent, MatSidenavModule, MatButtonModule, MatIconModule, RouterLink, RouterLinkActive, ResponsiveDirective, ContactBarComponent],
   templateUrl: './navbar-mobile.component.html',
-  styleUrl: './navbar-mobile.component.scss'
+  styleUrl: './navbar-mobile.component.sass'
 })
 export class NavbarMobileComponent {
-  opened: boolean = false;
-  @ViewChild('drawer') drawer!: MatDrawer;
+  @Input() drawer!: MatDrawer;
+
+  close() {
+    this.drawer.close();
+  }
 }
