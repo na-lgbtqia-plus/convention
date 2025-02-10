854ac0a35a059cb7f6b8cf13a1f99f5dba688c4e
Ben <nalgbtqiaplus@gmail.com>
2025-02-11 00:59:42 +0100

    content: venue & activities page

--- src/app/pages/venue/venue.component.ts
+++ src/app/pages/venue/venue.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { ContactBarComponent } from '../../components/contact-bar/contact-bar.component';
 import { TitleBarWebComponent } from '../../components/title-bar-web/title-bar-web.component';
+import { NgOptimizedImage } from '@angular/common';
+import { ResponsiveDirective } from '../../directives/responsive.directive';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-venue',
-  imports: [],
+  imports: [NgOptimizedImage, ResponsiveDirective, MatButtonModule, MatIconModule],
   templateUrl: './venue.component.html',
   styleUrl: './venue.component.sass'
 })
