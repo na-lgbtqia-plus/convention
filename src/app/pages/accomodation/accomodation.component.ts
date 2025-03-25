fef1dbb72cce119f4c3eac07dc7f8c83158ee70d
Ben <nalgbtqiaplus@gmail.com>
2025-03-25 14:40:47 +0100

    added accomodation flyer for paris members

--- src/app/pages/accomodation/accomodation.component.ts
+++ src/app/pages/accomodation/accomodation.component.ts
@@ -4,10 +4,11 @@ import { TitleBarWebComponent } from '../../components/title-bar-web/title-bar-w
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ResponsiveDirective } from '../../directives/responsive.directive';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-accomodation',
-  imports: [MatIconModule, MatButtonModule, ResponsiveDirective],
+  imports: [MatIconModule, MatButtonModule, ResponsiveDirective, NgOptimizedImage],
   templateUrl: './accomodation.component.html',
   styleUrl: './accomodation.component.sass'
 })
