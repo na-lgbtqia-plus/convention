e36d5357cfbce433c0734dc5913ca91f46575541
Ben <nalgbtqiaplus@gmail.com>
2025-03-25 14:24:47 +0100

    program

--- src/app/pages/activities/activities.component.ts
+++ src/app/pages/activities/activities.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactBarComponent } from '../../components/contact-bar/contact-bar.component';
 import { TitleBarWebComponent } from '../../components/title-bar-web/title-bar-web.component';
+import { NgOptimizedImage } from '@angular/common';
+import { ResponsiveDirective } from '../../directives/responsive.directive';
 
 @Component({
   selector: 'app-activities',
-  imports: [],
+  imports: [ NgOptimizedImage, ResponsiveDirective ],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.sass'
 })
